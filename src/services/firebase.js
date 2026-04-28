bca4767e04cfafefead149385f4c74e5cc5de9bf
azcaio-dev <azevedocaio03@gmail.com>
2026-04-28 12:56:54 -0300

    move firebase config to env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,20 +1,17 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import { getAuth } from "firebase/auth"
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
-// 🔴 SUBSTITUA pelos dados do seu Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCMRSwZ0IAZd284yhAk091iLdMgxCHOsuo",
-  authDomain: "loja-labany2.firebaseapp.com",
-  projectId: "loja-labany2",
-  storageBucket: "loja-labany2.firebasestorage.app",
-  messagingSenderId: "1057706920841",
-  appId: "1:1057706920841:web:dad6ce4b538dc918452f58"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig)
 
-// Exporta serviços
 export const db = getFirestore(app)
 export const auth = getAuth(app)
